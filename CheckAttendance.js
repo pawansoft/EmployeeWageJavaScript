e29cc39e2ea5c0bc3d65e702728fe5a0ca4409f0
Pawan Kumar <pk.soft29@gmail.com>
2020-11-24 15:53:48 +0530

    Refactor : To add half day

--- CheckAttendance.js
+++ CheckAttendance.js
@@ -1,25 +1,32 @@
 const present = 1;
-const absent = 2;
+const halfDay = 2;
+const absent = 3;
 
 function checkAttendance(){
-    const attendance = Math.floor((Math.random() * 2) +1)
+    const attendance = Math.floor((Math.random() * 3) +1)
     if(attendance == present){
         return 'FullDay'
+    }
+    else if(attendance ==  halfDay ){
+        return 'HalfDay';
     }
     else if(attendance ==  absent ){
         return 'Absent';
     }
 }
 
-
 function calculateDailyWage(){
     const WAGE_PER_HOUR = 20;
     const HOUR_IN_DAY = 8;
+    const HALF_DAY = 4;
 
     const attend = checkAttendance();
 
     if(attend == 'FullDay'){
         return HOUR_IN_DAY * WAGE_PER_HOUR;
+    }
+    else if(attend == 'HalfDay'){
+        return HALF_DAY * WAGE_PER_HOUR;
     }
     else if(attend == 'Absent'){
         return 0;
