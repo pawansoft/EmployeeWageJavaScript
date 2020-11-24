a659d2afd82c9904fc6ad9a26d38abd2caca1032
Pawan Kumar <pk.soft29@gmail.com>
2020-11-24 19:45:38 +0530

    Added : Total working hour in month

--- CheckAttendance.js
+++ CheckAttendance.js
@@ -4,17 +4,18 @@ function calculateDailyWage(){
     const HALF_DAY = 4;
     var totalWorkingDay = 0;
     var totalWage = 0;
-    while(totalWorkingDay < 20){
+    var totalWorkingHour = 0;
+    while(totalWorkingDay < 20 && totalWorkingHour < 100){
 
         const attendance = Math.floor((Math.random() * 3) +1);
 
         if(attendance == 1){
-            totalWage = totalWage + HOUR_IN_DAY * WAGE_PER_HOUR;
+            totalWage = (HOUR_IN_DAY * WAGE_PER_HOUR) + totalWage;
             totalWorkingDay = totalWorkingDay + 1;
         }
         else if(attendance == 2){
-            totalWage = totalWage + HALF_DAY * WAGE_PER_HOUR;
-            totalWorkingDay = totalWorkingDay + 1;
+            totalWage = (HALF_DAY * WAGE_PER_HOUR) + totalWage ;
+            totalWorkingHour = totalWorkingHour + 4;
         }
         else if(attendance == 3){
         }
