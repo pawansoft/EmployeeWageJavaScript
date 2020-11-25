6e418e168b6dee8b66b7921648ede7ad00ba5778
Pawan Kumar <pk.soft29@gmail.com>
2020-11-25 07:14:27 +0530

    Refactor : To store daily wage along with total wage

--- CheckAttendance.js
+++ CheckAttendance.js
@@ -11,6 +11,8 @@ function calculateDailyWage(WAGE_PER_HOUR, HOUR_IN_DAY, HALF_DAY){
     var totalWorkingDay = 0;
     var totalWage = 0;
     var totalWorkingHour = 0;
+    var arrOfDailyWage = new Array(30);
+    var i = 0;
     while(totalWorkingDay < 20 && totalWorkingHour < 100){
 
         const attendance = Math.floor((Math.random() * 3) +1);
@@ -18,15 +20,19 @@ function calculateDailyWage(WAGE_PER_HOUR, HOUR_IN_DAY, HALF_DAY){
         if(attendance == 1){
             totalWage = (HOUR_IN_DAY * WAGE_PER_HOUR) + totalWage;
             totalWorkingDay = totalWorkingDay + 1;
+            arrOfDailyWage[i] = totalWage;
         }
         else if(attendance == 2){
             totalWage = (HALF_DAY * WAGE_PER_HOUR) + totalWage ;
             totalWorkingHour = totalWorkingHour + 4;
+            arrOfDailyWage[i] = totalWage;
         }
         else if(attendance == 3){
+            arrOfDailyWage[i] = "Absent";
         }
+        i++;
     }
-    return totalWage;
+    return arrOfDailyWage;
 }
 
 const employeeWage = new EmployeeWage(20, 8, 4);
@@ -34,3 +40,7 @@ console.log(employeeWage.HALF_DAY);
 
 const attend = calculateDailyWage(employeeWage.WAGE_PER_HOUR, employeeWage.HOUR_IN_DAY, employeeWage.HALF_DAY);
 console.log(attend);
+
+console.log('-------------------------------------------------');
+console.log("Total Wage For This Month : " + attend[attend.length - 1]);
+console.log('-------------------------------------------------');
