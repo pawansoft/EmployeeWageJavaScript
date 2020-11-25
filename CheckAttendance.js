c322e064cfdc623ece45411e75b54d5f25c52937
Pawan Kumar <pk.soft29@gmail.com>
2020-11-25 06:33:43 +0530

    Refator : To get Work Hour

--- CheckAttendance.js
+++ CheckAttendance.js
@@ -1,7 +1,13 @@
-function calculateDailyWage(){
-    const WAGE_PER_HOUR = 20;
-    const HOUR_IN_DAY = 8;
-    const HALF_DAY = 4;
+class EmployeeWage{
+
+    constructor(WAGE_PER_HOUR, HOUR_IN_DAY, HALF_DAY){
+        this.WAGE_PER_HOUR = WAGE_PER_HOUR;
+        this.HOUR_IN_DAY = HOUR_IN_DAY;
+        this.HALF_DAY = HALF_DAY;
+    }
+}
+
+function calculateDailyWage(WAGE_PER_HOUR, HOUR_IN_DAY, HALF_DAY){
     var totalWorkingDay = 0;
     var totalWage = 0;
     var totalWorkingHour = 0;
@@ -23,5 +29,8 @@ function calculateDailyWage(){
     return totalWage;
 }
 
-const attend = calculateDailyWage();
+const employeeWage = new EmployeeWage(20, 8, 4);
+console.log(employeeWage.HALF_DAY);
+
+const attend = calculateDailyWage(employeeWage.WAGE_PER_HOUR, employeeWage.HOUR_IN_DAY, employeeWage.HALF_DAY);
 console.log(attend);
